6e2eaf045031992720ad3ffb506437fe48edf178
QWEEEEEERTY <211688@astanait.edu.kz>
2024-12-23 14:16:57 +0500

    middleware empty response body

--- src/api/middlewares.py
+++ src/api/middlewares.py
@@ -18,7 +18,10 @@ class LoggingMiddleware(BaseHTTPMiddleware):
         response_body = b""
         async for chunk in response.body_iterator:
             response_body += chunk
-        response_body_str = response_body.decode('utf-8') if response_body else "No body"
+        try:
+            response_body_str = response_body.decode('utf-8') if response_body else "No body"
+        except UnicodeDecodeError:
+            response_body_str = "Failed to decode response body"
 
         rebuilt_response = Response(
             content=response_body,
